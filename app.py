3706433d25e91fc667fe7f955551d28759ca1010
Jaren Glenn <jarenglenn@gmail.com>
2020-04-28 22:37:34 -0600

    HTML structure complete; fonts and colors chosen

--- app.py
+++ app.py
@@ -1,46 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from flask import Flask, render_template, url_for
+from flask_assets import Environment, Bundle
 
-app = Flask(__name__) # instane of our app
-app.config.update(SQLALCHEMY_DATABASE_URI='sqlite:///' +
-                  os.path.join(basedir, 'app.db'), # defined location of db file
-                  SQLALCHEMY_TRACK_MODIFICATIONS=False)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+app = Flask(__name__)
 
-from models import Message # must be imported after the db is declared, otherwie ImportError
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('main.scss', filters='pyscss', output='css/main.css')
+assets.register('scss_all', scss)
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Message': Message}
-
-# pylint: disable=no-member
-@app.route('/', methods=["GET", "POST"]) # routes are defined using decorators
+@app.route("/")
 def home():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        return render_template('index.html', num=request.form['num']) # pass context to the Jinja template for rendering
-
-@app.route("/messages")
-def messages():
-    messages = Message.query.all() # FROM message SELECT *;
-    return render_template('messages.html', messages=messages)
-
-@app.route("/messages/new", methods=['GET', 'POST'])
-def new_message():
-    if request.method == 'GET':
-        return render_template('message_new.html')
-    elif request.method == 'POST':
-        message = Message(text=request.form['message'])
-        db.session.add(message)
-        db.session.commit()
-        return redirect(url_for('thing', user='jaren', year='2020', month=1, day=1))
-
-@app.route("/messages/<int:_id>")
-def show_message(_id):
-    message = Message.query.get(_id)
-    return render_template("message_show.html", message=message)
+    return render_template("index.html")
