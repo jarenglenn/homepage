0a3d7865883e9bf38dfe2dcfb05965593c304384
Jaren Glenn <jarenglenn@gmail.com>
2020-04-29 11:16:59 -0600

    basic grid layout

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 assets = Environment(app)
 assets.url = app.static_url_path
-scss = Bundle('main.scss', filters='pyscss', output='css/main.css')
+
+scss = Bundle(
+  'main.scss', 
+  filters='pyscss', 
+  output='css/main.css', 
+  depends=("*.scss", "utilities/*.scss")) 
+
 assets.register('scss_all', scss)
 
 @app.route("/")
